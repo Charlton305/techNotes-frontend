1ae841c3cc4db99593ec2c503811c04b5a2574a8
Lee Charlton <Leejcharlton@hotmail.com>
2024-09-19 08:05:21 +0100

    Ready to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 import App from './App.jsx'
 import './index.css'
 import { store } from "./app/store.js"
 import { Provider } from "react-redux"
+
+if (process.env.NODE_ENV === "production") disableReactDevTools()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
